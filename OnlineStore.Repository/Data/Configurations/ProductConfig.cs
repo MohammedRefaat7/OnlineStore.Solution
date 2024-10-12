2b6adc933ac162730a4e5e9e6b2e1551aecf24da
MohammedRefaat7 <mr8024072@gmail.com>
2024-10-12 23:32:16 +0200

    AddMigration(InitialCreate)

--- OnlineStore.Repository/Data/Configurations/ProductConfig.cs
+++ OnlineStore.Repository/Data/Configurations/ProductConfig.cs
@@ -20,6 +20,8 @@ namespace OnlineStore.Repository.Data.Configurations
 			builder.Property(P => P.Description).IsRequired();
 			builder.Property(P => P.PictureUrl).IsRequired();
 
+			builder.Property(P => P.Price).HasColumnType("decimal(18,2)");
+
 			builder.HasOne(P => P.ProductBrand)
 				.WithMany()
 				.HasForeignKey(P => P.ProductBrandId);
