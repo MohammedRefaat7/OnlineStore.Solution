67de2d33325817baf93a4f459c2406e9d98b389e
MohammedRefaat7 <mr8024072@gmail.com>
2024-11-03 23:18:42 +0200

    AccountsController(Register Endpoint)

---
+++ OnlineStore.API/Controllers/AccountsController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OnlineStore.API.DTOs;
+using OnlineStore.API.Errors;
+using OnlineStore.Core.Models.Identity;
+
+namespace OnlineStore.API.Controllers
+{
+	public class AccountsController : APIBaseController
+	{
+		private readonly UserManager<AppUser> _userManager;
+
+		public AccountsController(UserManager<AppUser> userManager )
+        {
+			_userManager = userManager;
+		}
+
+
+        // Register
+        [HttpPost("Register")]
+		public async Task<ActionResult<UserDto>> Register(RegisterDto model)
+		{
+			var User = new AppUser()
+			{
+				Email = model.Email,
+				UserName = model.Email.Split('@')[0],
+				DisplayName = model.DisplayName,
+				PhoneNumber = model.PhoneNumber,
+			};
+			var Result = await _userManager.CreateAsync(User, model.Password);
+
+			if (!Result.Succeeded)
+				return BadRequest(new ApiErrorResponse(400));
+			else
+			{
+				var ReturnedUser = new UserDto()
+				{
+					Email = User.Email,
+					DisplayName = User.DisplayName,
+					Token = "ThisWillBeToken!!"
+				};
+				return Ok(ReturnedUser);
+			}
+		}
+	}
+}
---
+++ OnlineStore.API/DTOs/RegisterDto.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineStore.API.DTOs
+{
+	public class RegisterDto
+	{
+		[Required]
+		public string DisplayName { get; set; }
+
+		[Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Phone(ErrorMessage = "Invalid Phone Number")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [RegularExpression("(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$" ,
+                      ErrorMessage = "Password Must Contains 1 Uppercase, 1 Lowercase, 1 Digit, 1 Spicial Character")]
+        public string Password{ get; set; }
+
+
+
+    }
+}
