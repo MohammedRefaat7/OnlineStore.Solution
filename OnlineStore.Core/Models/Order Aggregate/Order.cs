1070084a60be22f427b665bceddeaf32850b559e
MohammedRefaat7 <mr8024072@gmail.com>
2024-11-10 21:39:04 +0200

    OrderModuleEntities

---
+++ OnlineStore.Core/Models/Order Aggregate/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Core.Models.Order_Aggregate
+{
+	public class Order : BaseEntity
+	{
+        public string BuyerEmail { get; set; }
+		public DateTimeOffset DateTimeOffset { get; set; } = DateTimeOffset.Now;
+		public OrderStatus status { get; set; } = OrderStatus.Pending;
+        public Address ShippingAddress { get; set; }
+        public DeliveryMethod DeliveryMethod { get; set; }
+        public ICollection<OrderItem> Items { get; set; } = new HashSet<OrderItem>();
+        public decimal SubTotal { get; set; }
+        public decimal GetTotal() 
+            => SubTotal + DeliveryMethod.Cost;
+        public string PaymentIntentId { get; set; }
+    }
+}
---
+++ OnlineStore.Core/Models/Order Aggregate/OrderItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Core.Models.Order_Aggregate
+{
+	public class OrderItem : BaseEntity
+	{
+        public ProductItemOrdered Product { get; set; }
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+
+    }
+}
