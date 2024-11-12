514f497d69a012dd47d4fd9379d3f64624fe0910
MohammedRefaat7 <mr8024072@gmail.com>
2024-11-12 21:58:38 +0200

    (OrderService)Interface

---
+++ OnlineStore.Core/IServices/IOrderService.cs
@@ -0,0 +1,21 @@
+﻿using OnlineStore.Core.Models.Order_Aggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Core.IServices
+{
+	public interface IOrderService
+	{
+		// Create Order
+		Task<Order> CreateOrderAsync(string BuyerEmail, string BasketId, int DeliveryMethodId, Address ShippingAddress);
+
+		// Get All Orders For Specific User
+		Task<IReadOnlyList<Order>> GetOrdersForSpecificUserAsync(string BuyerEmail);
+
+		// Get Order By ID For Specific User
+		Task<Order> GetOrderByIdForSpecificUserAsync(string BuyerEmail, int OrderId);
+	}
+}
